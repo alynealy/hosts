08e0af2e8196f15f289b9d87afc7d5fb2448f0f9
ALina <alinaelenapup26@gmail.com>
2024-11-18 00:34:36 -0800

    am modificat scrpitul

--- validate_hosts.sh
+++ validate_hosts.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+validate_ip() {
+    local host=$1
+    local ip=$2
+    local dns_server=$3
+
+    if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Validating $host ($ip) using DNS server $dns_server..."
+        nslookup $host $dns_server | grep -q "$ip" && echo "Valid" || echo "Invalid"
+    else
+        echo "Invalid IP format: $ip"
+    fi
+}
+
 while IFS= read -r line; do
-	ip=$(echo "$line" | awk '{print $1}')
-	if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-		echo "Ip valid: $ip"
-	else
-		echo  "Ip invalid : $ip"
-	fi
-done </etc/hosts
+    host=$(echo "$line" | awk '{print $2}')
+    ip=$(echo "$line" | awk '{print $1}')
+    validate_ip "$host" "$ip" "8.8.8.8"
+done < /etc/hosts
